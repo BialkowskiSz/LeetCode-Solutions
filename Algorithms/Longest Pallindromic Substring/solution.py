7df9c60a801a67d24adb74051fca86e97da91081
BialkowskiSz <bialkowski.sz@gmail.com>
2018-05-11 22:00:56 +0100

    One solution.

---
+++ Algorithms/Longest Pallindromic Substring/solution.py
@@ -0,0 +1,43 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        length = len(s)
+        if length <= 1:
+            return s
+        
+        longest  = 1
+        longestString = s[0]
+        indexMap = {}
+        
+        for index, value in enumerate(s):
+            if value in indexMap:
+                indexMap[value].append(index + 1)
+            else:
+                indexMap[value] = [index + 1]
+        
+        for index, value in enumerate(s):
+            indexMapPop = indexMap[value][::-1]
+            for indexMapPopValue in indexMapPop:
+                potentialPallindrome = s[index : indexMapPopValue :]
+                lenPallindrome = self.isPallindrome(potentialPallindrome)
+                if lenPallindrome is not None:
+                    if lenPallindrome > longest:
+                        longest = lenPallindrome
+                        longestString = potentialPallindrome
+        
+        return longestString
+            
+        
+    
+    def isPallindrome(self, s):
+        length = len(s)
+        if length <= 1:
+            return length
+        
+        check = (s == s[::-1])
+        if check:
+            return length
+        return None
